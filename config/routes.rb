8060abd83ad2ed5e8f71ca161a60685c0cea4dbd
asamih <a.hirose78@gmail.com>
2020-04-10 14:12:52 +0900

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'users/show'
   scope "(:locale)" do
     resources :books
+    devise_for :users, controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations"
+    }
+    resources :users, only: [:show]
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "books#index"
 end
---
+++ db/migrate/20200329130948_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :postcode, :integer
+    add_column :users, :address, :string
+    add_column :users, :introduction, :text
+  end
+end
