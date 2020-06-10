f879d3b846ebb5f5d32d870b0801b6fc34583590
asamih <a.hirose78@gmail.com>
2020-06-11 08:03:25 +0900

    不要なスペースを削除

--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::FollowersController < ApplicationController
   def index
-    @user  = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
     @users = @user.followers
   end
 end
