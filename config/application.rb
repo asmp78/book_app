ee94f463fcb088ecf629b2f95d66840ed6d8d027
asamih <a.hirose78@gmail.com>
2020-06-13 15:58:45 +0900

    Ajaxの設定を削除

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module BooksApp
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
-    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
